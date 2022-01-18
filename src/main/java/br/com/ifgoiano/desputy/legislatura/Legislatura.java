96fa00d310dc3947478ad0155239005a337c344d
Wanderson <wandersonfelipeplus13@gmail.com>
2022-01-18 18:04:53 -0300

    model legislatura

---
+++ src/main/java/br/com/ifgoiano/desputy/legislatura/Legislatura.java
@@ -0,0 +1,121 @@
+package br.com.ifgoiano.desputy.legislatura;
+
+import java.io.Serializable;
+import java.sql.Date;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import br.com.ifgoiano.desputy.usuario.Usuario;
+
+@Entity
+@Table(name = "legislatura")
+public class Legislatura implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column(name = "idlegislatura")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int idLegislatura;
+
+	@Column(name = "uri")
+	private String uri;
+
+	@Column(name = "datainicio")
+	private String dataInicio;
+
+	@Column(name = "datafim")
+	private Date dataFim;
+
+	@Column(name = "anoeleicao")
+	private int anoEleicao;
+
+	@ManyToOne
+	@OnDelete(action = OnDeleteAction.CASCADE)
+	@JoinColumn(nullable = false)
+	@Column(name = "usuario")
+	private Usuario usuario;
+
+	public int getIdLegislatura() {
+		return idLegislatura;
+	}
+
+	public void setIdLegislatura(int idLegislatura) {
+		this.idLegislatura = idLegislatura;
+	}
+
+	public String getUri() {
+		return uri;
+	}
+
+	public void setUri(String uri) {
+		this.uri = uri;
+	}
+
+	public String getDataInicio() {
+		return dataInicio;
+	}
+
+	public void setDataInicio(String dataInicio) {
+		this.dataInicio = dataInicio;
+	}
+
+	public Date getDataFim() {
+		return dataFim;
+	}
+
+	public void setDataFim(Date dataFim) {
+		this.dataFim = dataFim;
+	}
+
+	public int getAnoEleicao() {
+		return anoEleicao;
+	}
+
+	public void setAnoEleicao(int anoEleicao) {
+		this.anoEleicao = anoEleicao;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(anoEleicao, dataFim, dataInicio, idLegislatura, uri, usuario);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Legislatura other = (Legislatura) obj;
+		return anoEleicao == other.anoEleicao && Objects.equals(dataFim, other.dataFim)
+				&& Objects.equals(dataInicio, other.dataInicio) && idLegislatura == other.idLegislatura
+				&& Objects.equals(uri, other.uri) && Objects.equals(usuario, other.usuario);
+	}
+
+}
